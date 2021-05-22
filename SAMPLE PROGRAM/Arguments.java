9adeaf7de95bc7708e05e180cc84750036c0fe4e
KARTHIK D <57323477+KARTHIK-KD@users.noreply.github.com>
2021-05-22 02:44:58 -0700

    Add files via upload

---
+++ SAMPLE PROGRAM/Arguments.java
@@ -0,0 +1,8 @@
+package com.onebill.program;
+
+public class Arguments {
+	public static void main(String[] args) {
+		System.out.println(args[0]);
+	}
+
+}
---
+++ SAMPLE PROGRAM/HelloWorld2.java
@@ -0,0 +1,9 @@
+package com.onebill.program;
+
+public class HelloWorld2 {
+	public static void main(String arg[])
+	{
+		System.out.print("Hello World Example2");
+	}
+
+}
